cf7bb70768b3acb839c057927e0fcb2fca21f99b
jonlin84 <jon.c.lin@gmail.com>
2018-08-26 17:25:01 -0700

    add to webapp.py

--- app/4_html_dropdown/webapp.py
+++ app/4_html_dropdown/webapp.py
@@ -6,5 +6,12 @@ def index():
     
     return render_template('nbabet.html')
 
+@app.route('/submit')
+def submit():
+    return '''
+    <form action="/predict" method='POST'>
+        <input type (GET BOTH TEAM COLUMNS AND SPREAD INPUT) ='input_data' />
+    '''
+    
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True)
+    app.run(host='0.0.0.0',port=8080, debug=True)
