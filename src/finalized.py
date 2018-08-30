5aa4992cf40b448179ff05926b172cfcec4c9f62
jonlin84 <jon.c.lin@gmail.com>
2018-08-29 23:55:53 -0700

    add thresholder function

--- src/finalized.py
+++ src/finalized.py
@@ -66,50 +66,66 @@ def transform_spread(spread,teams,year,box,team_avg,rolling_avg=5):
                     'avg_opp_fta_last_5','avg_opp_orb_last_5','avg_opp_drb_last_5',
                     'avg_opp_trb_last_5','avg_opp_ast_last_5','avg_opp_stl_last_5',
                     'avg_opp_blk_last_5','avg_opp_tov_last_5','avg_opp_pf_last_5','avg_opp_pts_last_5']
-    avg_5_no_pct_diff = ['avg_mp_last_5_diff','avg_fg_last_5_diff','avg_fga_last_5_diff','avg_fg3_last_5_diff',
- 'avg_fg3a_last_5_diff','avg_ft_last_5_diff','avg_fta_last_5_diff','avg_orb_last_5_diff','avg_drb_last_5_diff',
- 'avg_trb_last_5_diff','avg_ast_last_5_diff','avg_stl_last_5_diff','avg_blk_last_5_diff','avg_tov_last_5_diff',
- 'avg_pf_last_5_diff','avg_pts_last_5_diff','avg_opp_mp_last_5_diff','avg_opp_fg_last_5_diff',
- 'avg_opp_fga_last_5_diff','avg_opp_fg3_last_5_diff','avg_opp_fg3a_last_5_diff','avg_opp_ft_last_5_diff',
- 'avg_opp_fta_last_5_diff','avg_opp_orb_last_5_diff','avg_opp_drb_last_5_diff','avg_opp_trb_last_5_diff',
- 'avg_opp_ast_last_5_diff','avg_opp_stl_last_5_diff','avg_opp_blk_last_5_diff','avg_opp_tov_last_5_diff',
- 'avg_opp_pf_last_5_diff','avg_opp_pts_last_5_diff']
-    spread = spread.copy()
+    
+    avg_5_no_pct_diff = ['avg_mp_last_5_diff','avg_fg_last_5_diff','avg_fga_last_5_diff',
+                        'avg_fg3_last_5_diff','avg_fg3a_last_5_diff','avg_ft_last_5_diff',
+                        'avg_fta_last_5_diff','avg_orb_last_5_diff','avg_drb_last_5_diff',
+                        'avg_trb_last_5_diff','avg_ast_last_5_diff','avg_stl_last_5_diff',
+                        'avg_blk_last_5_diff','avg_tov_last_5_diff','avg_pf_last_5_diff',
+                        'avg_pts_last_5_diff','avg_opp_mp_last_5_diff','avg_opp_fg_last_5_diff',
+                        'avg_opp_fga_last_5_diff','avg_opp_fg3_last_5_diff','avg_opp_fg3a_last_5_diff',
+                        'avg_opp_ft_last_5_diff','avg_opp_fta_last_5_diff','avg_opp_orb_last_5_diff',
+                        'avg_opp_drb_last_5_diff','avg_opp_trb_last_5_diff','avg_opp_ast_last_5_diff',
+                        'avg_opp_stl_last_5_diff','avg_opp_blk_last_5_diff','avg_opp_tov_last_5_diff',
+                        'avg_opp_pf_last_5_diff','avg_opp_pts_last_5_diff']
+
+    spread = spread[spread['year']==year].copy()
     spread.index = range(len(spread))
     spread_final = pd.DataFrame()
     
     rolling_avg_season = create_df_season(teams,year,box,team_avg,rolling_avg=5)
     for i in range(len(spread)):
-        gameid = spread.gameid[i]
-        team = spread.team[i]
-        opp = spread.opp[i]
-        team_stat = rolling_avg_season.loc[(rolling_avg_season['gameid']==gameid) & (rolling_avg_season['team']==team)]
-        opp_stat = rolling_avg_season.loc[(rolling_avg_season['gameid']==gameid)& (rolling_avg_season['team']==opp)]
-        diff = team_stat[avg_5_no_pct] - opp_stat[avg_5_no_pct]
+        gameid = spread.game_id[i]
+        team = spread['team'][i]
+        opp = spread['opp'][i]
+        team_stat = rolling_avg_season[(rolling_avg_season['game_id']==gameid) & (rolling_avg_season['team']==team)]
+        opp_stat = rolling_avg_season[(rolling_avg_season['game_id']==gameid)& (rolling_avg_season['team']==opp)]
+        opp_stats = pd.concat([opp_stat[avg_5_no_pct][:16],opp_stat[avg_5_no_pct][16:]],axis=0).values
+        team_ats = rolling_avg_season['ats_record'].loc[(rolling_avg_season['game_id']==gameid)&(rolling_avg_season['team']==team)].values
+        opp_ats = rolling_avg_season['ats_record'].loc[(rolling_avg_season['game_id']==gameid)&(rolling_avg_season['team']==opp)].values
+        #print(team_ats)
+        diff = pd.DataFrame(team_stat[avg_5_no_pct].values - opp_stats)
         diff.columns = avg_5_no_pct_diff
+        diff['team_ats'] = team_ats
+        diff['opp_ats'] = opp_ats
         spread_final = pd.concat([spread_final,diff],axis=0)
     spread_final.index=range(len(spread_final))
 
     return pd.concat([spread,spread_final],axis=1)
 
-drop_columns = ['team', 'year', 'mp', 'fg', 'fga', 'fg_pct', 'fg3', 'fg3a', 'fg3_pct',
-       'ft', 'fta', 'ft_pct', 'orb', 'drb', 'trb', 'ast', 'stl', 'blk', 'tov',
-       'pf', 'pts', 'opp_mp', 'opp_fg', 'opp_fga', 'opp_fg_pct', 'opp_fg3',
-       'opp_fg3a', 'opp_fg3_pct', 'opp_ft', 'opp_fta', 'opp_ft_pct', 'opp_orb',
-       'opp_drb', 'opp_trb', 'opp_ast', 'opp_stl', 'opp_blk', 'opp_tov',
-       'opp_pf', 'opp_pts', 't', 'y', 'g', 'date', 'opp', 'result', 'score', 'ou', 'total','score_diff', 'spread_diff', 'gameid', 'game_id']
-
-
 def create_model_data(trans_df):
     '''
     returns X,y
     '''
-    drop_columns = ['team', 'home','year', 'mp', 'fg', 'fga', 'fg_pct', 'fg3', 'fg3a', 'fg3_pct',
+    drop_columns = ['team','home', 'year', 'mp', 'fg', 'fga', 'fg_pct', 'fg3', 'fg3a', 'fg3_pct',
        'ft', 'fta', 'ft_pct', 'orb', 'drb', 'trb', 'ast', 'stl', 'blk', 'tov',
        'pf', 'pts', 'opp_mp', 'opp_fg', 'opp_fga', 'opp_fg_pct', 'opp_fg3',
        'opp_fg3a', 'opp_fg3_pct', 'opp_ft', 'opp_fta', 'opp_ft_pct', 'opp_orb',
        'opp_drb', 'opp_trb', 'opp_ast', 'opp_stl', 'opp_blk', 'opp_tov',
-       'opp_pf', 'opp_pts', 't', 'y', 'g', 'date', 'opp', 'result', 'score', 'ou', 'total','score_diff', 'spread_diff', 'gameid', 'game_id']
+       'opp_pf', 'opp_pts', 't', 'y', 'date', 'opp', 'result', 'score', 'ou', 'total','score_diff', 'spread_diff', 'gameid', 'game_id','s_double','s_less_3']
     X = trans_df.drop(columns=drop_columns)
-    y = trans_df.ats
+    y = trans_df[['ats','g']]
     return X.drop(columns='ats'), y 
+
+def thresholder(predict_proba,y_test,threshold=.55):
+    right = 0
+    wrong = 0
+    for i in range(len(predict_proba)):
+        if max(predict_proba[i] >= threshold):
+            if predict_proba[i][0] >= threshold and y_test[i] == 0.0:
+                right += 1
+            elif predict_proba[i][1] >= threshold and y_test[i] == 1.0:
+                right += 1
+            else:
+                wrong += 1
+    return right, wrong, (right/(right+wrong)), right+wrong
