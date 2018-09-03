09c3d278b27870934c9447314e29e0f6b301758d
jonlin84 <jon.c.lin@gmail.com>
2018-09-02 18:52:28 -0700

    add function

--- app/4_html_dropdown/webapp.py
+++ app/4_html_dropdown/webapp.py
@@ -6,12 +6,13 @@ def index():
     
     return render_template('nbabet.html')
 
-@app.route('/submit')
-def submit():
-    return '''
-    <form action="/predict" method='POST'>
-        <input type (GET BOTH TEAM COLUMNS AND SPREAD INPUT) ='input_data' />
-    '''
+@app.route('/solve', methods=['POST'])
+def solve():
+    user_data = request.json
+    home_team, away_team, spread = user_data['team1'], user_data['team2'], user_data['spread']
+    cover = _predict_proba(home_team, away_team, spread)
+    return jsonify({'Chance Home Team Covers!': cover)  
+
     
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080, debug=True)
