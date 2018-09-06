1c0d3db2c7316190769690eb25e12431a0dec784
jonlin84 <jon.c.lin@gmail.com>
2018-09-06 10:20:39 -0700

    make pep8 complient

--- modelmaker.py
+++ modelmaker.py
@@ -3,21 +3,26 @@ import pandas as pd
 import numpy as np
 import pickle
 
+
 class SpreadModel(object):
+
     '''
     creates model with specified inputs
     '''
+
     def __init__(self):
 
         self.box_df = pd.read_pickle('data/THEBIGDATAFRAME.pkl')
         self.team_avg = pd.read_pickle('data/team_avg.pkl')
         self.current_season = '2018'
-        self.teams = ['ATL', 'BOS', 'BRK', 'CHI', 'CHO', 'CLE', 'DAL', 'DEN',
+        self.teams = [
+                      'ATL', 'BOS', 'BRK', 'CHI', 'CHO', 'CLE', 'DAL', 'DEN',
                       'DET', 'GSW', 'HOU', 'IND', 'LAC', 'LAL', 'MEM', 'MIA',
                       'MIL', 'MIN', 'NOP', 'NYK', 'OKC', 'ORL', 'PHI', 'PHO',
                       'POR', 'SAC', 'SAS', 'TOR', 'UTA', 'WAS']
 
-        self.avg_list_no_pct = ['mp', 'fg', 'fga', 'fg3', 'fg3a', 'ft', 'fta',
+        self.avg_list_no_pct = [
+                                'mp', 'fg', 'fga', 'fg3', 'fg3a', 'ft', 'fta',
                                 'orb', 'drb', 'trb', 'ast', 'stl', 'blk',
                                 'tov', 'pf', 'pts', 'opp_mp', 'opp_fg',
                                 'opp_fga', 'opp_fg3', 'opp_fg3a', 'opp_ft',
@@ -25,7 +30,8 @@ class SpreadModel(object):
                                 'opp_trb', 'opp_ast', 'opp_stl', 'opp_blk',
                                 'opp_tov', 'opp_pf', 'opp_pts']
 
-        self.avg_5_no_pct = ['avg_mp_last_5', 'avg_fg_last_5',
+        self.avg_5_no_pct = [
+                            'avg_mp_last_5', 'avg_fg_last_5',
                             'avg_fga_last_5', 'avg_fg3_last_5',
                             'avg_fg3a_last_5', 'avg_ft_last_5',
                             'avg_fta_last_5', 'avg_orb_last_5',
@@ -42,7 +48,8 @@ class SpreadModel(object):
                             'avg_opp_blk_last_5', 'avg_opp_tov_last_5',
                             'avg_opp_pf_last_5', 'avg_opp_pts_last_5']
 
-        self.avg_5_no_pct_diff = ['team_mp_last_avg_diff',
+        self.avg_5_no_pct_diff = [
+                        'team_mp_last_avg_diff',
                         'team_fg_last_avg_diff', 'team_fga_last_avg_diff',
                         'team_fg3_last_avg_diff', 'team_fg3a_last_avg_diff',
                         'team_ft_last_avg_diff', 'team_fta_last_avg_diff',
@@ -60,7 +67,8 @@ class SpreadModel(object):
                         'opp_tov_last_avg_diff', 'opp_pf_last_avg_diff',
                         'opp_pts_last_avg_diff']
 
-        self.drop_columns = ['team', 'home', 'year', 'mp', 'fg', 'fga',
+        self.drop_columns = [
+                        'team', 'home', 'year', 'mp', 'fg', 'fga',
                         'fg_pct', 'fg3', 'fg3a', 'fg3_pct',
                         'ft', 'fta', 'ft_pct', 'orb', 'drb',
                         'trb', 'ast', 'stl', 'blk', 'tov',
@@ -92,15 +100,17 @@ class SpreadModel(object):
         box_df = pd.read_pickle('data/THEBIGDATAFRAME.pkl')
         sp = box_df[(box_df['team'] == team) & (box_df['year'] == year)].copy()
         sp.sort_values('g', inplace=True)
-        ats_record = np.insert((np.cumsum(sp.ats.values[:len(sp)-1]))
-                                / range(1, len(sp)), 0, 0.0)
+        ats_record = np.insert((
+                        np.cumsum(sp.ats.values[:len(sp)-1]))
+                        / range(1, len(sp)), 0, 0.0)
         sp['ats_record'] = ats_record
         sp.index = range(len(sp))
         roll_avg = sp[self.avg_list_no_pct].rolling(
                                             rolling_avg, min_periods=1).mean()
         roll_avg.columns = self.avg_5_no_pct
         if int(year) > 2014:
-            insertion = pd.DataFrame(data=self.team_avg[team]
+            insertion = pd.DataFrame(
+                                    data=self.team_avg[team]
                                     [(str(int(year)-1))]
                                     [0][self.avg_list_no_pct]).T
         else:
@@ -108,14 +118,14 @@ class SpreadModel(object):
         insertion.columns = self.avg_5_no_pct
         roll_avg = pd.concat([insertion, roll_avg[:-1]], axis=0)
         roll_avg.index = range(len(roll_avg))
-        team_df = box_df[(box_df['team'] == team) & (box_df['year'] == year)]\
-                         .copy()
+        team_df = box_df[
+                (box_df['team'] == team) & (box_df['year'] == year)].copy()
         team_df.sort_values('g', inplace=True)
         team_df.index = range(len(team_df))
         return pd.concat([team_df, roll_avg, sp['ats_record']], axis=1)
 
-    def _transform_spread(self, year, rolling_avg, without_first_games,
-                            spread_home_team):
+    def _transform_spread(
+            self, year, rolling_avg, without_first_games, spread_home_team):
         '''
         transforms dataframe:
             e.x. spread_home_team = True, year='2014', rolling_avg = 6
@@ -135,12 +145,12 @@ class SpreadModel(object):
             gameid = spread['game_id'][i]
             team = spread['team'][i]
             opp = spread['opp'][i]
-            team_stat = rolling_avg_season[(rolling_avg_season['game_id'] ==
-                                gameid)
+            team_stat = rolling_avg_season[
+                                (rolling_avg_season['game_id'] == gameid)
                                 & (rolling_avg_season['team'] == team)].copy()
-            opp_stat = rolling_avg_season[(rolling_avg_season['game_id']
-                                == gameid) & (rolling_avg_season['team'] ==
-                                opp)].copy()
+            opp_stat = rolling_avg_season[
+                                (rolling_avg_season['game_id'] == gameid)
+                                & (rolling_avg_season['team'] == opp)].copy()
             opp_stats = opp_stat[self.avg_5_no_pct]
             home_stats = team_stat[self.avg_5_no_pct]
             h_off = (home_stats.values[0][:16] + opp_stats.values[0][16:]) / 2
@@ -154,8 +164,9 @@ class SpreadModel(object):
         spread_final.index = range(len(spread_final))
         return pd.concat([spread, spread_final], axis=1)
 
-    def create_model_data(self, year='2014', rolling_avg=6,
-                            without_first_games=True, home_spread_team=True):
+    def create_model_data(
+            self, year='2014', rolling_avg=6,
+            without_first_games=True, home_spread_team=True):
         '''
         returns transformed dataframe with X,y with or without first games
         '''
@@ -175,18 +186,18 @@ class SpreadModel(object):
         Takes home team, away team as 3 letter string, spread as float
         returns transformed dataframe for predicting
         '''
-        home_rolling = self._current_get_rolling_avg(home,
-                                            self.current_season, 6)
-        away_rolling = self._current_get_rolling_avg(away,
-                                            self.current_season, 6)
+        home_rolling = self._current_get_rolling_avg(
+                                        home, self.current_season, 6)
+        away_rolling = self._current_get_rolling_avg(
+                                    away, self.current_season, 6)
         home_data = home_rolling.iloc[len(away_rolling)-1].copy()
         away_data = away_rolling.iloc[len(away_rolling)-1].copy()
         away_stats = away_data[self.avg_5_no_pct].copy()
         home_stats = home_data[self.avg_5_no_pct].copy()
         h_off = (home_stats[:16].values + away_stats[16:].values) / 2
         o_off = (away_stats[:16].values + home_stats[16:].values) / 2
-        diff = pd.concat([pd.DataFrame(h_off).T,
-                         pd.DataFrame(o_off).T], axis=1)
+        diff = pd.concat(
+                        [pd.DataFrame(h_off).T, pd.DataFrame(o_off).T], axis=1)
         diff.columns = self.avg_5_no_pct_diff
         diff['team_ats'] = home_data['ats_record']
         diff['opp_ats'] = away_data['ats_record']
@@ -200,8 +211,9 @@ class SpreadModel(object):
         opp_5 = away_data['team_last_5']
         team_b2b = home_data['team_b2b']
         opp_b2b = away_data['team_b2b']
-        data = {'g': [g], 'spread': [spread], 'team_last_5': [team_5],
-                        'opp_last_5': [opp_5], 'team_b2b': [team_b2b],
-                        'opp_b2b': [opp_b2b]}
+        data = {
+                'g': [g], 'spread': [spread], 'team_last_5': [team_5],
+                'opp_last_5': [opp_5], 'team_b2b': [team_b2b],
+                'opp_b2b': [opp_b2b]}
         df = pd.DataFrame(data=data)
         return pd.concat([df, diff], axis=1)
--- src/scrape.py
+++ src/scrape.py
@@ -11,8 +11,8 @@ import numpy as np
 
 class Scrape():
     '''
-    This is used to grab boxscore data for each NBA team puts in boxscore collection the chosen
-    mongodb name
+    This is used to grab boxscore data for each NBA team puts
+    in boxscore collection the chose a mongodb name
     Pass a list of years you want scraped ex. ['2009','2010','2011']
     '''
 
@@ -22,25 +22,31 @@ class Scrape():
         self.client = MongoClient()
         self.years = years
         self.baseurl = 'https://www.basketball-reference.com/'
-        self.teams = ['ATL', 'BOS', 'BRK', 'CHI', 'CHO', 'CLE', 'DAL', 'DEN',
+        self.teams = [
+                    'ATL', 'BOS', 'BRK', 'CHI', 'CHO', 'CLE', 'DAL', 'DEN',
                     'DET', 'GSW', 'HOU', 'IND', 'LAC', 'LAL', 'MEM', 'MIA',
                     'MIL', 'MIN', 'NOP', 'NYK', 'OKC', 'ORL', 'PHI', 'PHO',
                     'POR', 'SAC', 'SAS', 'TOR', 'UTA', 'WAS']
 
-        self.team_dic = {'Dallas': 'DAL', 'Boston': 'BOS', 'Toronto': 'TOR',
-                        'Denver': 'DEN', 'Philadelphia': 'PHI', 'New York': 'NYK',
-                        'Orlando': 'ORL', 'Cleveland': 'CLE', 'Detroit': 'DET',
+        self.team_dic = {
+                        'Dallas': 'DAL', 'Boston': 'BOS', 'Toronto': 'TOR',
+                        'Denver': 'DEN', 'Philadelphia': 'PHI', 'New York':
+                        'NYK', 'Orlando': 'ORL', 'Cleveland': 'CLE', 'Detroit':
+                        'DET',
                         'Miami': 'MIA', 'Charlotte': 'CHO', 'Houston': 'HOU',
                         'San Antonio': 'SAS', 'LA Clippers': 'LAC',
                         'Washington': 'WAS', 'Oklahoma City': 'OKC',
                         'Milwaukee': 'MIL', 'Phoenix': 'PHO',
                         'Sacramento': 'SAC', 'New Orleans': 'NOP',
-                        'Indiana': 'IND', 'Portland': 'POR', 'Brooklyn': 'BRK',
+                        'Indiana': 'IND', 'Portland': 'POR', 'Brooklyn':
+                        'BRK',
                         'Golden State': 'GSW', 'Chicago': 'CHI', 'LA Lakers':
-                        'LAL', 'Memphis': 'MEM', 'Atlanta': 'ATL', 'Utah': 'UTA',
+                        'LAL', 'Memphis': 'MEM', 'Atlanta': 'ATL', 'Utah':
+                        'UTA',
                         'Minnesota': 'MIN'}
 
-        self.sp = {'BOS': '20722', 'PHI': '20731', 'BRK': '20749', 'NYK':
+        self.sp = {
+            'BOS': '20722', 'PHI': '20731', 'BRK': '20749', 'NYK':
             '20747', 'TOR': '20742', 'DET': '20743', 'CLE': '20735', 'CHI':
             '20732', 'IND': '20737', 'MIL': '20725', 'CHO': '20751', 'MIA':
             '20726', 'ORL': '20750', 'ATL': '20734', 'WAS': '20746', 'OKC':
@@ -49,7 +55,8 @@ class Scrape():
             '20736', 'GSW': '20741', 'DAL': '20727', 'MEM': '20729', 'HOU':
             '20740', 'SAS': '20724', 'NOP': '20733'}
 
-        self.baselink = 'https://www.oddsshark.com/stats/gamelog/basketball/nba/'
+        self.baselink = \
+            'https://www.oddsshark.com/stats/gamelog/basketball/nba/'
 
     def build_db(self):
         big_list = self._url_list_generator()
@@ -88,7 +95,8 @@ class Scrape():
             biglist[team] = {}
             biglist[team]['year'] = {}
             for year in self.years:
-                biglist[team]['year'][year] = self._box_score_url_creator_bbref(team, year)
+                biglist[team]['year'][year] = \
+                    self._box_score_url_creator_bbref(team, year)
         return biglist
 
     def _soup_maker(self, dct: dict):
@@ -103,7 +111,8 @@ class Scrape():
                 url = dct[team]['year'][year][0]
                 r = requests.get(url)
                 soup = BeautifulSoup(r.content, 'html.parser')
-                boxscores[team]['year'][year] = self._get_box_score_url(soup, games=82)
+                boxscores[team]['year'][year] = \
+                    self._get_box_score_url(soup, games=82)
                 time.sleep(3)
         return boxscores
 
@@ -113,7 +122,8 @@ class Scrape():
                 for items in dct[team]['year'][year]:
                     r = requests.get(items)
                     time.sleep(3)
-                    boxscore = {'team': team,
+                    boxscore = {
+                                'team': team,
                                 'year': year,
                                 'url': items,
                                 'content': r.content}
@@ -125,7 +135,8 @@ class Scrape():
                 url = self.baselink + self.sp[team] + '/' + year
                 r = requests.get(url)
                 time.sleep(10)
-                spreadlist = {'team': team,
+                spreadlist = {
+                        'team': team,
                         'year': year,
                         'url': url,
                         'content': r.content}
