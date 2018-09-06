2b3e99eaaec0e8b1be075ffabb39c1ee9fef9f07
jonlin84 <jon.c.lin@gmail.com>
2018-09-06 09:53:20 -0700

    edit syntax

--- webapp.py
+++ webapp.py
@@ -5,7 +5,7 @@ import re
 from modelmaker import SpreadModel
 sp = SpreadModel()
 
-model = pickle.load(open( "data/model.pkl", "rb" ))
+model = pickle.load(open("data/model.pkl", "rb"))
 
 app = Flask(__name__)
 
@@ -18,15 +18,17 @@ def index():
 @app.route('/solve', methods=['POST'])
 def solve():
     user_data = request.json
-    home, away, spread = user_data['home'], user_data['away'], user_data['spread']
+    home, away, spread = user_data['home'], user_data['away'],\
+                    user_data['spread']
     prob = _predict_proba(home, away, spread)
-    return jsonify({'Probability Home Team Wins': prob*100}) 
+    return jsonify({'Probability Home Team Wins': prob*100})
 
 
-def _predict_proba(home,away,spread):
-    train = sp.matchup_predict_data(home,away,spread)
-    prob =  model.predict_prob(train)[0] * 100
+def _predict_proba(home, away, spread):
+    train = sp.matchup_predict_data(home, away, spread)
+    prob = model.predict_prob(train)[0] * 100
     return prob
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
